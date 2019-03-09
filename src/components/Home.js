25eef3984011c35fa1a331aa0e248d6d10c5d340
Nicole L. Hopkins <nicolehopkins@pursuit.org>
2019-03-09 16:56:38 -0500

    adding react page and basic home page

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+
+class Home extends Component {
+    render() {
+        return (
+            <>
+            <div className='header'>
+            <h1>StyleBox</h1>
+          </div>
+          <div className='body'>
+              Body
+          </div>
+          </>
+        )
+    }
+
+}
+
+
+export default Home;
