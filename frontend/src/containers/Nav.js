d0484e38b4bd1b574da037895cca964ceee277a6
Nicole L. Hopkins <nicolehopkins@pursuit.org>
2019-03-24 16:07:57 -0400

    hopefully deleting .DS_Store files from repo + updating gitignore file

--- frontend/src/containers/Nav.js
+++ frontend/src/containers/Nav.js
@@ -20,36 +20,45 @@ export default class Nav extends React.Component {
     }
 
     render() {
-    // const { user } = this.state;
+    const { user } = this.state;
 
-    // const publicPage = 
+    const userNav = <nav className="nav-bar">
+                        <div className='logo'>
+                            <a href='/'><img src='https://tbncdn.freelogodesign.org/985834d1-1891-43a0-ba41-76dcc9cea295.png?1552172962507' alt='logo'/></a>
+                        </div>
+                        <ul className="nav-tags links">
+                            <li>
+                                <Link className="links" to="/products">Search <FaSearch /></Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="links" to="/cart">My Box <FiShoppingCart /></Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="links" to="/logout">Logout</Link>
+                            </li>
+                        </ul>
+                    </nav>
 
-    // if (user) {
-    //     return 
 
-    // }
+    const defaultNav = <nav className="nav-bar">
+                            <div className='logo'>
+                                <a href='/'><img src='https://tbncdn.freelogodesign.org/985834d1-1891-43a0-ba41-76dcc9cea295.png?1552172962507' alt='logo'/></a>
+                            </div>
+                            <ul className="nav-tags links">
+                                <li>
+                                    <Link className="links" to="/products">Search <FaSearch /></Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="links" to="/login">Sign In <FiUser /></Link>
+                                </li>
+                            </ul>
+                        </nav>
 
-    return (
-            <nav className="nav-bar">
-                <div className='logo'>
-                    <a href='/'><img src='https://tbncdn.freelogodesign.org/985834d1-1891-43a0-ba41-76dcc9cea295.png?1552172962507' alt='logo'/></a>
-                </div>
-                <ul className="nav-tags links">
-                    <li>
-                        <Link className="links" to="/products">Search <FaSearch /></Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="links" to="/login">Sign In <FiUser /></Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="links" to="/logout">Logout</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="links" to="/cart">My Box <FiShoppingCart /></Link>
-                    </li>
-                </ul>
-            </nav>
+    if (user) {
+        return userNav;
+    } else {
+        return defaultNav;
+    }
 
-    )
 }
 }
