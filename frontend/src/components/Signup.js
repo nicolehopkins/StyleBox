706cf39e18acfc0a34a6cfa304946503016d2a8c
Nicole L. Hopkins <nicolehopkins@pursuit.org>
2019-03-30 15:30:13 -0400

    adding new reactstrap to signup page

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import firebase from '../firebase';
 import AuthContext from '../contexts/auth';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import '../styling/Signup.css'
 
 export default class Signup extends Component {
 
@@ -31,23 +33,40 @@ export default class Signup extends Component {
   
   
     render() {
+
       const { email, password, error } = this.state;
       const displayError = error === '' ? '' : <div className="alert alert-danger" role="alert">{error}</div>
-      const displayForm = <>
-                <h1>Sign Up</h1>
-                {displayError}
-                <form>
-                  <div className="form-group">
-                    <label htmlFor="exampleInputEmail1">Email</label>
-                    <input type="email" className="form-control" aria-describedby="emailHelp" placeholder="Enter email" name="email" value={email} onChange={this.handleChange} />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="exampleInputPassword1">Password</label>
-                    <input type="password" className="form-control" placeholder="Password" value={password} name="password" onChange={this.handleChange} />
-                  </div>
-                  <button type="submit" className="btn btn-primary" onClick={this.handleSubmit}>Sign Up</button>
-                </form>
-                </>;
+      const displayForm = <div>
+                            <h1>Sign Up</h1>
+                            {displayError}
+                            <Form inline>
+                            <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                              <Label for="exampleEmail" className="mr-sm-2">Email</Label>
+                              <Input value={email} type="email" name="email" id="exampleEmail" placeholder="something@idk.cool" onChange={this.handleChange}/>
+                            </FormGroup>
+                            <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                              <Label for="examplePassword" className="mr-sm-2">Password</Label>
+                              <Input value={password} type="password" name="password" id="examplePassword" placeholder="don't tell!" onChange={this.handleChange} />
+                            </FormGroup>
+                            <Button type="submit" onClick={this.handleSubmit}>Submit</Button>
+                            </Form>
+                          </div>
+      
+      
+      
+      // <>
+      //           <form>
+      //             <div className="form-group">
+      //               <label htmlFor="exampleInputEmail1">Email</label>
+      //               <input type="email" className="form-control" aria-describedby="emailHelp" placeholder="Enter email" name="email" value={email} onChange={this.handleChange} />
+      //             </div>
+      //             <div className="form-group">
+      //               <label htmlFor="exampleInputPassword1">Password</label>
+      //               <input type="password" className="form-control" placeholder="Password" value={password} name="password" onChange={this.handleChange} />
+      //             </div>
+      //             <button type="submit" className="btn btn-primary" onClick={this.handleSubmit}>Sign Up</button>
+      //           </form>
+      //           </>;
   
       return (
         <AuthContext.Consumer >
@@ -62,7 +81,6 @@ export default class Signup extends Component {
             }
           }
         </AuthContext.Consumer>
-        
       );
     }
 }
