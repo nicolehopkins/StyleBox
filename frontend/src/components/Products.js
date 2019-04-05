c18ecf95a8c3e5b9c86009b7c4de117626c8d2ce
Nicole L. Hopkins <nicolehopkins@pursuit.org>
2019-04-04 22:34:42 -0400

    adding state for products component

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, Button} from 'reactstrap';
 import { FaRegThumbsUp } from 'react-icons/fa';
@@ -6,29 +6,23 @@ import { FiHeart } from 'react-icons/fi';
 import '../styling/Products.css'
 
 
-const Products = (props) => {
-    // this.state = {
-    //     images: [],
-    //     productName: '', 
-    //     modelNumber: '', 
-    //     description: '', 
-    //     isLiked: false,
-    // }
+export default class Products extends Component {
+    constructor(props) {
+        super(props)
 
+        this.state = {
+            images: [],
+            productName: '', 
+            modelNumber: '', 
+            description: '', 
+            isLiked: false,
+        }
+    }
 
-    return (
-        <div className='container'>
-            <div className='product-card'>
-                <Card >
-                <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-                <CardBody>
-                <CardTitle><h2>Product Name</h2></CardTitle>
-                    <CardSubtitle><h3>Model Number</h3></CardSubtitle>
-                    <CardText>Product description. yadda yadda yadda...</CardText>
-                    <Button>I like  <FiHeart /></Button>
-                </CardBody>
-                </Card>
-            </div>
+    render() {
+
+    const { images, productName, modelNumber, description, isLiked } = this.state
+    return ( <>
             <div className='product-card'>
                 <Card >
                 <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
@@ -84,8 +78,8 @@ const Products = (props) => {
                 </CardBody>
                 </Card>
             </div>
-        </div>
+            </>
     )
 }
 
-export default Products;
+}
