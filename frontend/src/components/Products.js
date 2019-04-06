eccb0905b7a01016bcdd777fff2080e05912a0aa
Nicole L. Hopkins <nicolehopkins@pursuit.org>
2019-04-06 11:08:58 -0400

    adding categories to product page

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -11,10 +11,10 @@ export default class Products extends Component {
         super(props)
 
         this.state = {
-            image: 'https://ae01.alicdn.com/kf/HTB1DCulhyCYBuNkSnaVq6AMsVXaa/Stephen-Hawking-Formal-Quotes-Intelligence-Design-Funny-T-Shirt-for-Men-and-Women-Unisex-Graphic-Premium.jpg_640x640.jpg',
-            productName: 'intelligence tshirt', 
-            modelNumber: '1TZ34', 
-            description: 'white crew neck tshirt', 
+            images: ['https://ae01.alicdn.com/kf/HTB1DCulhyCYBuNkSnaVq6AMsVXaa/Stephen-Hawking-Formal-Quotes-Intelligence-Design-Funny-T-Shirt-for-Men-and-Women-Unisex-Graphic-Premium.jpg_640x640.jpg'],
+            productName: ['intelligence tshirt'], 
+            modelNumber: ['1TZ34'], 
+            description: ['white crew neck tshirt'], 
             isLiked: false,
         }
     }
@@ -23,6 +23,31 @@ export default class Products extends Component {
 
     const { image, productName, modelNumber, description, isLiked } = this.state
     return ( <>
+            <div className='category'><h3>Tops</h3></div>
+            <div className='product-card'>
+                <Card >
+                <CardImg top width="100%" src={image} alt="Card image cap" />
+                <CardBody>
+                <CardTitle><h2>{productName}</h2></CardTitle>
+                    <CardSubtitle><h3>{modelNumber}</h3></CardSubtitle>
+                    <CardText>{description}</CardText>
+                    <Button>I like  <FiHeart /></Button>
+                </CardBody>
+                </Card>
+            </div>
+            <div className='category'><h3>Bottoms</h3></div>
+            <div className='product-card'>
+                <Card >
+                <CardImg top width="100%" src={image} alt="Card image cap" />
+                <CardBody>
+                <CardTitle><h2>{productName}</h2></CardTitle>
+                    <CardSubtitle><h3>{modelNumber}</h3></CardSubtitle>
+                    <CardText>{description}</CardText>
+                    <Button>I like  <FiHeart /></Button>
+                </CardBody>
+                </Card>
+            </div>
+            <div className='category'><h3>Accessories</h3></div>
             <div className='product-card'>
                 <Card >
                 <CardImg top width="100%" src={image} alt="Card image cap" />
