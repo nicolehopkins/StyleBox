3b644ffa746373e275280aa7f80825435376fbb6
Nicole L. Hopkins <nicolehopkins@pursuit.org>
2019-04-07 12:41:29 -0400

    positioning button on homepage

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -11,9 +11,7 @@ export default class Home extends Component {
     render() {
         return (
             <div className='container'>
-   
                 <Button className='get-started-button' href='/signup' exact component={Signup} >Get Started</Button>
-
                 <div className="images">
                     <img alt='style1' src='https://images.unsplash.com/photo-1528991435120-e73e05a58897?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=clamp&w=600&q=60' />
                     <img alt='style2' src='https://images.unsplash.com/photo-1542776575-f1623249ce85?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=clamp&w=600&q=60' />
