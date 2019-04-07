555c48f4c736afaea17312ed62d4fecb902fc08e
Nicole L. Hopkins <nicolehopkins@pursuit.org>
2019-04-07 13:05:11 -0400

    simplifying product card and adding file for dummy data

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -11,31 +11,29 @@ export default class Products extends Component {
         super(props)
 
         this.state = {
-            images: ['https://ae01.alicdn.com/kf/HTB1DCulhyCYBuNkSnaVq6AMsVXaa/Stephen-Hawking-Formal-Quotes-Intelligence-Design-Funny-T-Shirt-for-Men-and-Women-Unisex-Graphic-Premium.jpg_640x640.jpg'],
-            productName: ['intelligence tshirt'], 
-            modelNumber: ['1TZ34'], 
-            description: ['white crew neck tshirt'], 
+            image: ['https://ae01.alicdn.com/kf/HTB1DCulhyCYBuNkSnaVq6AMsVXaa/Stephen-Hawking-Formal-Quotes-Intelligence-Design-Funny-T-Shirt-for-Men-and-Women-Unisex-Graphic-Premium.jpg_640x640.jpg'],
+            productName: ['blah blah blah'],  
             isLiked: false,
         }
     }
 
+
+
     render() {
 
-    const { image, productName, modelNumber, description, isLiked } = this.state
+    const { image, productName, isLiked } = this.state
     return ( <>
-            <div className='category'><h3>Tops</h3></div>
+            <div className='product-container'><h3>Like Products to Add Them to Your Box!</h3></div>
             <div className='product-card'>
                 <Card >
                 <CardImg top width="100%" src={image} alt="Card image cap" />
                 <CardBody>
                 <CardTitle><h2>{productName}</h2></CardTitle>
-                    <CardSubtitle><h3>{modelNumber}</h3></CardSubtitle>
-                    <CardText>{description}</CardText>
-                    <Button>I like  <FiHeart /></Button>
+                    <Button value={isLiked}>I like  <FiHeart /></Button>
                 </CardBody>
                 </Card>
             </div>
-            <div className='category'><h3>Bottoms</h3></div>
+            {/* <div className='category'><h3>Bottoms</h3></div>
             <div className='product-card'>
                 <Card >
                 <CardImg top width="100%" src={image} alt="Card image cap" />
@@ -58,7 +56,7 @@ export default class Products extends Component {
                     <Button>I like  <FiHeart /></Button>
                 </CardBody>
                 </Card>
-            </div>
+            </div> */}
             </>
     )
 }
