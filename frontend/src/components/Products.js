231c0889e31ae3ca0b323891f223e93a26b9cf96
Nicole L. Hopkins <nicolehopkins@pursuit.org>
2019-04-08 23:32:08 -0400

    adding initial code for api call and testing

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardBody, CardTitle, Button} from 'reactstrap';
 import { FiHeart } from 'react-icons/fi';
 import '../styling/Products.css'
 import Data from '../sources/DummyData'
+import Axios from 'axios';
 
 
 export default class Products extends Component {
@@ -10,10 +11,25 @@ export default class Products extends Component {
         super(props)
 
         this.state = {
-            images: ['https://ae01.alicdn.com/kf/HTB1DCulhyCYBuNkSnaVq6AMsVXaa/Stephen-Hawking-Formal-Quotes-Intelligence-Design-Funny-T-Shirt-for-Men-and-Women-Unisex-Graphic-Premium.jpg_640x640.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkqBDUoB1H7K50Bm2nlFyIj4GB4WCbf54kzB97HuCs34bacX7g', 'https://cdn.cliqueinc.com/cache/posts/275492/spring-2019-trends-to-buy-now-275492-1545429888988-image.700x0c.jpg', 'https://i.pinimg.com/originals/15/20/70/152070eff331ebd628fed9751f739015.jpg'],
-            productNames: ['intelligence tshirt', 'mens two-toned polo shirt', 'womens golden oversized blazer', 'womens tan pants'],  
+            productName: [],
+            images: [],  
             isLiked: false,
         }
+    }
+
+    componentDidMount() {
+        Axios.get('http://localhost:3001/products/?offset=0&limit=5')
+            .then(response => {
+                console.log(response.data)
+                return response;
+            })
+            // .then(productsArr => {
+            //     let productName = [];
+            //     let images = [];
+            //     productsArr.forEach((e, i) => {
+            //         productName
+            //     });
+            // })
     }
 
 
@@ -22,12 +38,12 @@ export default class Products extends Component {
 
     const { images, productNames, isLiked } = this.state
     return ( <>
-            <div className='product-container'><h3>Like Products to Add Them to Your Box!</h3></div>
+            <div className='product-container'><h2>Latest Trends</h2></div>
             <div className='product-card'>
                 <Card >
                 <CardImg top width="100%" src={images[0]} alt="Card image cap" />
                 <CardBody>
-                <CardTitle><h2>{productNames[0]}</h2></CardTitle>
+                <CardTitle><h2></h2></CardTitle>
                     <Button value={isLiked}>I like  <FiHeart /></Button>
                 </CardBody>
                 </Card>
