79897c8418b933ef68cc2e8c7f3df93dc8b59cdd
Nicole L. Hopkins <nicolehopkins@pursuit.org>
2019-10-13 09:22:34 -0400

    deleting unused pics + styling How It Works component

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -40,7 +40,7 @@ export default class Home extends Component {
           </div>
         </div>
         <HowItWorks />
-        <Footer />
+        {/* <Footer /> */}
       </>
     );
   }
--- frontend/src/containers/HowItWorks.js
+++ frontend/src/containers/HowItWorks.js
@@ -1,47 +1,31 @@
 import React from "react";
 
+// IMAGES
+import Man from '../sources/man.svg';
+import Box from  '../sources/box-heart.svg';
+import Outfit from  '../sources/outfit.svg';
+
 const HowItWorks = () => {
   return (
-    <div className="section " style={{ minHeight: '100vh', padding: '5%', alignContent: 'center' }}>
-      <div className="row">
+    <div className="section cyan lighten-5 rubik" style={{ minHeight: '100vh', padding: '5%', alignContent: 'center' }}>
+      <div className="row" style={{ padding: '3%' }}>
+        <h1>HOW IT WORKS</h1>
+      </div>
+      <div className='row' style={{ padding: '2%' }}>
         <div className="col s4">
-          <h2>Step 1</h2>
-          <p>
-            This will be the about us section where we tell more about Stylebox
-            and what we have to offer.
-          </p>
-          <p>Blah blah blah blah blah blah</p>
-          <p>Blah blah blah blah blah blah</p>
-          <p>Blah blah blah blah blah blah</p>
-          <p>Blah blah blah blah blah blah</p>
-          <p>Blah blah blah blah blah blah</p>
-          <p>Blah blah blah blah blah blah</p>
+          <h3>Step 1.</h3>
+          <img src={Man} style={{ maxWidth: '300px', padding: '10%'}} alt='gentleman' />
+            <p style={{ fontSize: '20px' }}>Tell us about yourself.</p>
         </div>
         <div className="col s4">
-          <h2>Step 2</h2>
-          <p>
-            This will be the about us section where we tell more about Stylebox
-            and what we have to offer.
-          </p>
-          <p>Blah blah blah blah blah blah</p>
-          <p>Blah blah blah blah blah blah</p>
-          <p>Blah blah blah blah blah blah</p>
-          <p>Blah blah blah blah blah blah</p>
-          <p>Blah blah blah blah blah blah</p>
-          <p>Blah blah blah blah blah blah</p>
+        <h3>Step 2.</h3>
+          <img src={Box} style={{ maxWidth: '300px', padding: '10%'}} alt='box' />
+          <p style={{ fontSize: '20px' }}>Browse our  your personal style.</p>
         </div>
         <div className="col s4">
-          <h2>Step 3</h2>
-          <p>
-            This will be the about us section where we tell more about Stylebox
-            and what we have to offer.
-          </p>
-          <p>Blah blah blah blah blah blah</p>
-          <p>Blah blah blah blah blah blah</p>
-          <p>Blah blah blah blah blah blah</p>
-          <p>Blah blah blah blah blah blah</p>
-          <p>Blah blah blah blah blah blah</p>
-          <p>Blah blah blah blah blah blah</p>
+          <h3>Step 3.</h3>
+          <img src={Outfit} style={{ maxWidth: '300px', padding: '1%'}} alt='outfit' />
+          <p style={{ fontSize: '20px' }}>Success, your Stylebox is here! Now, you have the freshest clothes in your closet.</p>
         </div>
       </div>
     </div>
