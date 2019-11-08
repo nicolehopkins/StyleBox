97d41a9ce58c14d1bb73d0af0b36ec56cc4cf198
Nicole L. Hopkins <nicolehopkins@pursuit.org>
2019-11-07 20:39:36 -0500

    changing logo styling on sign in Nav + changing logout component

--- frontend/src/containers/Logout.js
+++ frontend/src/containers/Logout.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react';
-import firebase from '../firebase';
+import React, { Component } from "react";
+import firebase from "../firebase";
 
 export default class Logout extends Component {
-
   componentDidMount() {
-    firebase.auth().signOut()
+    firebase.auth().signOut();
   }
 
   render() {
-    return <h1>Until We Meet Again...</h1>
+    return (
+      // <div>
+        <div
+          style={{
+            backgroundImage:
+              "url('https://images.unsplash.com/photo-1502159212845-f31a19546a5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80')",
+            height: "100vh",
+            width: "100%"
+          }}
+        >
+          <h1 style={{ color: 'white', textAlign: 'center' }}>Until We Meet Again...</h1>
+        </div>
+      // </div>
+    );
   }
 }
--- frontend/src/containers/Nav.js
+++ frontend/src/containers/Nav.js
@@ -89,13 +89,13 @@ export default class Nav extends Component {
           <div className="navbar-fixed" style={{ margin: "0 !important" }}>
             <nav className="white">
               <div className="nav-wrapper white">
-                <a
-                  href="/"
-                  className="brand-logo center long-cang"
-                  style={{ fontSize: "30px" }}
-                >
-                  Stylebox
-                </a>
+              <a
+                href="#/"
+                className="brand-logo center long-cang"
+                style={{ fontSize: "60px" }}
+              >
+                Stylebox
+              </a>
                 <a
                   href="/"
                   data-target="mobile-demo"
@@ -122,12 +122,15 @@ export default class Nav extends Component {
                     </a>
                   </li>
                   <li>
-                    <a href="#/cart">
+                  <a href="#/logout" className="rubik">
+                      LOG OUT
+                    </a>
+                    {/* <a href="#/cart">
                       <i className="material-icons">
                         shopping_basket
                         <span class="badge teal lighten-4">4</span>
                       </i>
-                    </a>
+                    </a> */}
                   </li>
                 </ul>
               </div>
